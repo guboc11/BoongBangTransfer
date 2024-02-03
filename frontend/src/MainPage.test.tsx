ed32d2e41a61c6a3040482191c2bcc1dc045ba40
guboc11 <guboc11@gmail.com>
2024-02-03 19:58:52 +0900

    update main page test

--- frontend/src/MainPage.test.tsx
+++ frontend/src/MainPage.test.tsx
@@ -1,7 +1,14 @@
-test("메뉴 컴포넌트 리스트가 있다.")
+import MainPage from "./MainPage"
+import { render, screen } from '@testing-library/react';
 
-test("수량 컴포넌트가 있다.")
+test("메뉴 컴포넌트 리스트가 있다.", () => {
+  render(<MainPage/>)
+  const productList = screen.getByTitle("ProductList")
+  expect(productList).toBeInTheDocument();
+})
 
-test("결제하기 버튼이 있다.")
+// test("수량 컴포넌트가 있다.")
 
-test("장바구니 컴포넌트가 있다.")
+// test("결제하기 버튼이 있다.")
+
+// test("장바구니 컴포넌트가 있다.")
