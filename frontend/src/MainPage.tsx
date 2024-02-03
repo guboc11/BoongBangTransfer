d41ccf0c0246c0720e510faac11b7219f1d68610
guboc11 <guboc11@gmail.com>
2024-02-03 19:59:22 +0900

    add MainPage, ProductList component

---
+++ frontend/src/MainPage.tsx
@@ -0,0 +1,12 @@
+import ProductList from "./ProductList";
+
+function MainPage() {
+  return(
+    <div>
+      <p>main page</p>
+      <ProductList></ProductList>
+    </div>
+  )
+}
+
+export default MainPage;
---
+++ frontend/src/ProductList.tsx
@@ -0,0 +1,7 @@
+export default function ProductList() {
+  return (
+    <div title="ProductList">
+      <p> product list </p>
+    </div>
+  )
+}
